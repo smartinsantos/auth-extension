eef4b4569f3b015eb07a0d31b514f64fd819189b
Sergio Martin <smartinsantos@gmail.com>
2023-03-12 21:20:20 -0500

    adding jwt token guard to all routes

--- src/iam/iam.module.ts
+++ src/iam/iam.module.ts
@@ -1,13 +1,15 @@
+import { APP_GUARD } from '@nestjs/core';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import jwtConfig from './config/jwt.config';
 import { HashingService } from './hashing/hashing.service';
 import { BcryptService } from './hashing/bcrypt.service';
 import { AuthenticationController } from './authentication/authentication.controller';
 import { AuthenticationService } from './authentication/authentication.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../users/entities/user.entity';
-import { JwtModule } from '@nestjs/jwt';
-import jwtConfig from './config/jwt.config';
-import { ConfigModule } from '@nestjs/config';
+import { AccessTokenGuard } from './authentication/guards/access-token/access-token.guard';
 
 @Module({
   imports: [
@@ -19,6 +21,10 @@ import { ConfigModule } from '@nestjs/config';
     {
       provide: HashingService,
       useClass: BcryptService,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: AccessTokenGuard,
     },
     AuthenticationService,
   ],
